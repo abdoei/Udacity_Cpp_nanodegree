c79d6b2ec3dcd9b924e6245828fe20f7a8f9b418
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-10 05:25:38 +0300

    03 CODE: The RoutePlanner Header

--- CppND-Route-Planning-Project/src/route_planner.cpp
+++ CppND-Route-Planning-Project/src/route_planner.cpp
@@ -1,6 +1,6 @@
 #include "route_planner.h"
 #include <algorithm>
-
+void print(){}
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-
+    void (*hello)() = print;
 }
--- CppND-Route-Planning-Project/src/route_planner.h
+++ CppND-Route-Planning-Project/src/route_planner.h
@@ -12,9 +12,16 @@ public: // constructor
   RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
   // Public variables and method declarations
 
+  // methods
+  float GetDistance() const noexcept { return distance;}
+
 private:
   // Private variables and method declarations
   RouteModel &m_Model;
+  RouteModel::Node *start_node, *end_node;
+  float distance; 
+
+  //methods
   
 };
 
