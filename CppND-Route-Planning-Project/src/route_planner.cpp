f30f2fb807107ab9a169864e955b87b8e24af939
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-11 06:13:51 +0300

    10 CODE: Complete A* Search

--- CppND-Route-Planning-Project/src/route_planner.cpp
+++ CppND-Route-Planning-Project/src/route_planner.cpp
@@ -28,8 +28,17 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
 // A* search function stub 
 void RoutePlanner::AStarSearch(){
-    end_node->parent = start_node;
-    m_Model.path = ConstructFinalPath(end_node);
+    start_node->visited = true;
+    open_list.emplace_back(start_node);
+    RouteModel::Node* current_node = nullptr;
+    while(open_list.size() > 0){
+        current_node = NextNode();
+        if(current_node->distance(*end_node) == 0){
+            m_Model.path = ConstructFinalPath(current_node);
+            return;
+        }
+        AddNeighbors(current_node);
+    }
 }
 
 float RoutePlanner::CalculateHValue(RouteModel::Node* nodePtr){
