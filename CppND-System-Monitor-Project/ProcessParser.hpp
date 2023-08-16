b3cbc5505c69849ae2c33f9a15137476d083ea06
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-16 06:09:52 +0300

    Added the ProcessParser and some tests

--- CppND-System-Monitor-Project/ProcessParser.hpp
+++ CppND-System-Monitor-Project/ProcessParser.hpp
@@ -1,30 +1,39 @@
 /* 
 // The required header files given by the course.
-#include <algorithm>
-#include <iostream>
+#include <algorithm> //
+#include <iostream> //
 #include <math.h>
 #include <thread>
 #include <chrono>
 #include <iterator>
-#include <string>
+#include <string> //
 #include <stdlib.h>
 #include <stdio.h>
-#include <vector>
-#include <fstream>
-#include <sstream>
+#include <vector> //
+#include <fstream> //
+#include <sstream> //
 #include <stdexcept>
-#include <cerrno>
-#include <cstring>
-#include <dirent.h>
+#include <cerrno> //
+#include <cstring> //
+#include <dirent.h> //
 #include <time.h>
 #include <unistd.h>
-#include <constants.h>
+#include <constants.h> //
 */
 
 #include <string>
 #include <vector>
-using std::string;
+#include <sstream>
+#include "constants.hpp"
+#include <fstream>
+#include <dirent.h>
+#include <cerrno>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
 
+using std::string;
+using std::vector;
 
 class ProcessParser {
     public:
@@ -45,3 +54,34 @@ class ProcessParser {
         static std::string printCpuStats(std::vector<std::string> values1, std::vector<std::string>values2);
 };
 
+string ProcessParser::getCmd(string pid){
+    string cmdline;
+    string path = Path::basePath() + pid + Path::cmdPath();
+    
+    std::ifstream stream(path);
+    std::getline(stream, cmdline);
+
+    return cmdline;
+}
+
+vector<string> ProcessParser::getPidList(){
+    vector<string> pidList;
+    DIR* directory;
+    dirent *entery;
+    
+    directory = opendir(Path::basePath().c_str());
+    if(directory){
+        while ((entery = readdir(directory)))
+        {
+            if(entery->d_type != DT_DIR) continue;             // if it is not a direcotry
+            if(std::all_of(entery->d_name, entery->d_name + strlen(entery->d_name), [](const char c){return isdigit(c);}))
+                pidList.emplace_back(entery->d_name); // if all of its name chars are digits
+        }
+        if(closedir(directory) == -1) // If not closed safely it returns -1
+            throw std::runtime_error(std::strerror(errno));
+    }
+    else
+        throw std::runtime_error(std::strerror(errno));
+
+    return pidList;
+}
--- CppND-System-Monitor-Project/testing.cpp
+++ CppND-System-Monitor-Project/testing.cpp
@@ -1,19 +1,41 @@
 #include "util.hpp"
 #include <iostream>
 #include <sstream>
+#include "ProcessParser.hpp"
 
 using namespace std;
 
 int main(){
+    // constants.hpp
     cout << Util::convertToTime(1000) << endl;
     cout << Util::getProgressBar("50") << endl;
-    
+
+/////////////////
+    cout << '\n';
+/////////////////
+
     auto myfile = Util::getStream("testing.cpp");
     if (myfile)
     {
         string line;
         while (getline(myfile, line)) cout << line << endl;
     }
+
+/////////////////
+    cout << '\n';
+/////////////////
+
+    // ProcessParser.hpp
+    cout << ProcessParser::getCmd("1660") << endl;
+    // this happens to be valid [when running on my machine] and gives "/usr/libexec/gvfs-udisks2-volume-monitor"
+
+/////////////////
+    cout << '\n';
+/////////////////
+
+    vector<string> processes = ProcessParser::getPidList();
+    for (string proc : processes)
+        cout << proc << " | ";
     
     return 0;
 }
