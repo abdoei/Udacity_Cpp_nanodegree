a5d02ae5d750ccf3aedfbae3b4b638df6898e755
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-24 01:58:51 +0300

    added the cmake task

--- CppND-System-Monitor-Project/build/src/CMakeFiles/SystemMonitor.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SystemMonitor.
-# This may be replaced when dependencies are built.
--- CppND-System-Monitor-Project/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/abdoeid/Data_2/Cpp_NanoDegree/UDACITY_CppND_v1/CODE/myGitHubRepo/Udacity_Cpp_nanodegree/CppND-System-Monitor-Project/src
-# Build directory: /media/abdoeid/Data_2/Cpp_NanoDegree/UDACITY_CppND_v1/CODE/myGitHubRepo/Udacity_Cpp_nanodegree/CppND-System-Monitor-Project/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
