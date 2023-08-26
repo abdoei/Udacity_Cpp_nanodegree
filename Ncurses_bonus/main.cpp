36d1b938d79904e24ce0a674d37d1ace0ac71bc9
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-26 22:17:07 +0300

    28 Bonus: ncurses Print and Modify

---
+++ Ncurses_bonus/main.cpp
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+#include <iostream>
+
+int main(){
+    initscr();
+    noecho();
+    addch('H' | A_UNDERLINE);
+    addch('E' | A_UNDERLINE);
+    addch('L' | A_BOLD);
+    addch('L' | A_BOLD);
+    addch('O' | A_BOLD);
+    getch();
+    endwin();
+    return 0;
+}
