cd3d4e5659a0e6c6170d4d0f7bf0108a3ec88666
Abdo Eid <abdoeid.eg@ieee.org>
2023-08-27 01:45:54 +0300

    finished the ncurses bonus tasks

--- Ncurses_bonus/main.cpp
+++ Ncurses_bonus/main.cpp
@@ -1,15 +1,38 @@
 #include <ncurses.h>
-#include <iostream>
+#include <unistd.h>
 
-int main(){
+WINDOW *create_win(int h, int w, int x, int y);
+
+int main() {
     initscr();
     noecho();
-    addch('H' | A_UNDERLINE);
-    addch('E' | A_UNDERLINE);
-    addch('L' | A_BOLD);
-    addch('L' | A_BOLD);
-    addch('O' | A_BOLD);
-    getch();
+    WINDOW* win = create_win(3, 10, 0, 0);
+    wrefresh(win);
+    sleep(1);
+    mvwprintw(win, 1, 2, "Hello");
+    wrefresh(win);
+    sleep(1);
+    WINDOW *win1 = create_win(10, 10, 0, 11);
+    WINDOW *win2 = create_win(10, 10, 9, 20);
+    wrefresh(win1);
+    wrefresh(win2);
+    while (1) {
+        // werase(win);
+        mvwprintw(win, 1, 2, "world");
+        wrefresh(win);
+        sleep(1);
+        wrefresh(win);
+        mvwprintw(win, 1, 2, "Hello");
+        wrefresh(win);
+        sleep(1);
+    }
     endwin();
     return 0;
+}
+
+WINDOW *create_win(int h, int w, int x, int y) {
+    WINDOW *localWin = newwin(h, w, x, y);
+    box(localWin, 0, 0);
+    wrefresh(localWin);
+    return localWin;
 }
